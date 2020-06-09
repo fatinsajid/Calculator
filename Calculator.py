05fc217d32e6fdd720adce0ade8d4eb686d5cf25
Fatin Ibtida Sajid <fatinsajid@gmail.com>
2020-06-10 04:03:44 +0600

    My first python project
    Signed-off-by: Fatin Ibtida Sajid <fatinsajid@gmail.com>

--- Calculator.py
+++ Calculator.py
@@ -1,388 +1,222 @@
 import tkinter
 from tkinter import *
+from tkinter.messagebox import *
+from math import sqrt
 
-val = ""
-operators = ""
-number = 0
+
 #creating button actions
-def button1clicked():
-    global val
-    val = val + "1"
-    disdata.set(val)
+def buttonsClicked(event):
+    a = event.widget
+    text = a["text"]
 
-def button2clicked():
-    global val
-    val = val + "2"
-    disdata.set(val)
-def button3clicked():
-    global val
-    val = val + "3"
-    disdata.set(val)
-def button4clicked():
-    global val
-    val = val + "4"
-    disdata.set(val)
-def button5clicked():
-    global val
-    val = val + "5"
-    disdata.set(val)
-def button6clicked():
-    global val
-    val = val + "6"
-    disdata.set(val)
-def button7clicked():
-    global val
-    val = val + "7"
-    disdata.set(val)
-def button8clicked():
-    global val
-    val = val + "8"
-    disdata.set(val)
-def button9clicked():
-    global val
-    val = val + "9"
-    disdata.set(val)
-def button0clicked():
-    global val
-    val = val + "0"
-    disdata.set(val)
-def buttonDotclicked():
-    global val
-    val = val + "."
-    disdata.set(val)
+    if text == "=":                     # if =  is clicked
+        try:
+            e = display.get()
+            res = eval(e)               # evaluates mathematical operators
+            display.delete(0, END)
+            display.insert(0, res)
+        except Exception:
+            showerror("Error", "Not Calculable")
+        return
+    elif text == "±":               # if ± is clicked
+        e = display.get()
+        res = (float(e) * -1)
+        display.delete(0, END)
+        display.insert(0, res)
+        return
+    elif text == "√":               # if √ is clicked
+        try:
+            e = display.get()
+            res = sqrt(float(e))
+            display.delete(0, END)
+            display.insert(0, res)
+        except Exception:
+            showerror("Error", "Not Calculable")
+        return
+    elif text == "pct":             # if pct is clicked
+        e = display.get()
+        res = float(e) / 100
+        display.delete(0, END)
+        display.insert(0, res)
+        return
+    display.insert(END, text)
 
-#creating operator actions
-def buttonplus():
-    global number
-    global operators
-    global val
-    number = int(val)
-    operators = "+"
-    val = val + "+"
-    disdata.set(val)
-def buttonminus():
-    global number
-    global operators
-    global val
-    number = int(val)
-    operators = "-"
-    val = val + "-"
-    disdata.set(val)
-def buttonmultiply():
-    global number
-    global operators
-    global val
-    number = int(val)
-    operators = "*"
-    val = val + "*"
-    disdata.set(val)
-def buttondivide():
-    global number
-    global operators
-    global val
-    number = int(val)
-    operators = "/"
-    val = val + "/"
-    disdata.set(val)
-def buttonpercent():
-    global number
-    global operators
-    global val
-    number = int(val)
-    operators = "%"
-    val = val + "%"
-    disdata.set(val)
-def buttonsquareroot():
-    global number
-    global operators
-    global val
-    number = int(val)
-    val = float(number ** 0.50)
-    disdata.set(val)
-def buttonnegpos():
-    global number
-    global operators
-    global val
-    number = int(val)
-    val = float(number * -1)
-    disdata.set(val)
-def buttonc():
-    global number
-    global operators
-    global val
-    number = 0
-    operators = ""
-    val = ""
-    disdata.set(val)
-def result():
-    global number
-    global operators
-    global val
-    val2 = val
-    if operators == "+":
-        x = int((val2.split("+")[1]))
-        c = number + x
-        disdata.set(c)
-        val = str(c)
-    elif operators == "-":
-        x = int((val2.split("-")[1]))
-        c = number - x
-        disdata.set(c)
-        val = str(c)
-    elif operators == "*":
-        x = int((val2.split("*")[1]))
-        c = number * x
-        disdata.set(c)
-        val = str(c)
-    elif operators == "%":
-        x = int((val2.split("%")[1]))
-        c = float((number * x)/100)
-        disdata.set(c)
-        val = str(c)
-    elif operators == "/":
-        x = int((val2.split("/")[1]))
-        if x == 0:
-            messagebox.showerror("Error")
-            A = 0
-            val = ""
-            disdata.set(val)
-        else:
-            c = float(number / x)
-            disdata.set(c)
-            val = str(c)
+def buttonC():
+    display.delete(0, END)
 
+    '''if I want to delete digits one by one
+    f = display.get()
+    f = f[0:len(f)-1]
+    display.delete(0, END)
+    display.insert(0, f)
+    '''
 
 root = tkinter.Tk()
-root.geometry("250x400+300+300")                #Measurement of the frame of the calculator
+root.geometry("232x286")                #Measurement of the frame of the calculator
 root.resizable(0,0)                             #Making the size unresizable
 root.title("Calculator")
 
-disdata = StringVar()
-
-display = Label(
+display = Entry(                        #Textfield for Calculator display
     root,
-    anchor = SE,
-    text = "0",
-    bg = "#C0C0C0",
-    font = ("Roboto", 20),
-    textvariable = disdata,
+    font = ("Roboto", 35),
+    bg = ("#C0C0C0"),
+    justify = RIGHT,
 )
 display.pack(expand = True, fill = "both")
 
-btnrow1 = Frame(root, bg = "#000000")           #Creating button row
-btnrow1.pack(expand = True, fill = "both")      #ensuring the button row size
+btnFrame = Frame(root, bg = "#000000")           #Creating button frame
+btnFrame.pack(expand = True, fill = "both")      #ensuring the button frame size
 
-btnrow2 = Frame(root, bg = "#000000")
-btnrow2.pack(expand = True, fill = "both")
+#creating buttons from 1-9 with grid row and column
+temp = 1
+for i in range(0,3):
+    for j in range(0,3):
+        btn1 = Button(
+            btnFrame,
+            text= str(temp),
+            font=("Roboto", 17),
+            width = 4,
+            bg="#000000", fg="white",
+            relief= GROOVE,
+            border=0,
+            activebackground="gray"
+        )
+        btn1.grid(row = i, column = j)
+        temp = temp + 1
+        btn1.bind("<Button-1>", buttonsClicked)
 
-btnrow3 = Frame(root, bg = "#000000")
-btnrow3.pack(expand = True, fill = "both")
 
-btnrow4 = Frame(root, bg = "#000000")
-btnrow4.pack(expand = True, fill = "both")
-
-btnrow5 = Frame(root, bg = "#000000")
-btnrow5.pack(expand = True, fill = "both")
-
-#Creating Bunttons
-btn1 = Button(
-    btnrow1,
-    text = "1",
-    font = ("Roboto", 10), bg = "#000000", fg = "white",
-    relief = GROOVE,
-    border = 0,
-    command = button1clicked
-)
-btn1.pack(side = LEFT, expand = True, fill = "both")
-
-btn2 = Button(
-    btnrow1,
-    text = "2",
-    font = ("Roboto", 10), bg = "#000000", fg = "white",
-    relief = GROOVE,
-    border = 0,
-    command = button2clicked
-)
-btn2.pack(side = LEFT, expand = True, fill = "both")
-
-btn3 = Button(
-    btnrow1,
-    text = "3",
-    font = ("Roboto", 10), bg = "#000000", fg = "white",
-    relief = GROOVE,
-    border = 0,
-    command = button3clicked
-)
-btn3.pack(side = LEFT, expand = True, fill = "both")
-
-btn4 = Button(
-    btnrow1,
-    text = "+",
-    font = ("Roboto", 10), bg = "#000000", fg = "white",
-    relief = GROOVE,
-    border = 0,
-    command = buttonplus
-)
-btn4.pack(side = LEFT, expand = True, fill = "both")
-
-btn5 = Button(
-    btnrow2,
-    text = "4",
-    font = ("Roboto", 10), bg = "#000000", fg = "white",
-    relief = GROOVE,
-    border = 0,
-    command = button4clicked
-)
-btn5.pack(side = LEFT, expand = True, fill = "both")
-
-btn6 = Button(
-    btnrow2,
-    text = "5",
-    font = ("Roboto", 10), bg = "#000000", fg = "white",
-    relief = GROOVE,
-    border = 0,
-    command = button5clicked
-)
-btn6.pack(side = LEFT, expand = True, fill = "both")
-
-btn7 = Button(
-    btnrow2,
-    text = "6",
-    font = ("Roboto", 10), bg = "#000000", fg = "white",
-    relief = GROOVE,
-    border = 0,
-    command = button6clicked
-)
-btn7.pack(side = LEFT, expand = True, fill = "both")
-
-btn8 = Button(
-    btnrow2,
-    text = "-",
-    font = ("Roboto", 10), bg = "#000000", fg = "white",
-    relief = GROOVE,
-    border = 0,
-    command = buttonminus
-)
-btn8.pack(side = LEFT, expand = True, fill = "both")
-
-btn9 = Button(
-    btnrow3,
-    text = "7",
-    font = ("Roboto", 10), bg = "#000000", fg = "white",
-    relief = GROOVE,
-    border = 0,
-    command = button7clicked
-)
-btn9.pack(side = LEFT, expand = True, fill = "both")
-
-btn10 = Button(
-    btnrow3,
-    text = "8",
-    font = ("Roboto", 10), bg = "#000000", fg = "white",
-    relief = GROOVE,
-    border = 0,
-    command = button8clicked
-)
-btn10.pack(side = LEFT, expand = True, fill = "both")
-
-btn11 = Button(
-    btnrow3,
-    text = "9",
-    font = ("Roboto", 10), bg = "#000000", fg = "white",
-    relief = GROOVE,
-    border = 0,
-    command = button9clicked
-)
-btn11.pack(side = LEFT, expand = True, fill = "both")
-
-btn12 = Button(
-    btnrow3,
-    text = "*",
-    font = ("Roboto", 10), bg = "#000000", fg = "white",
-    relief = GROOVE,
-    border = 0,
-    command = buttonmultiply
-)
-btn12.pack(side = LEFT, expand = True, fill = "both")
-
-btn13 = Button(
-    btnrow4,
-    text = ".",
-    font = ("Roboto", 10), bg = "#000000", fg = "white",
-    relief = GROOVE,
-    border = 0,
-    command = buttonDotclicked
-)
-btn13.pack(side = LEFT, expand = True, fill = "both")
-
-btn14 = Button(
-    btnrow4,
+#Creating other buttonswith grid row and column
+btn0 = Button(
+    btnFrame,
     text = "0",
-    font = ("Roboto", 10), bg = "#000000", fg = "white",
+    font = ("Roboto", 17), bg = "#000000", fg = "white",
     relief = GROOVE,
     border = 0,
-    command = button0clicked
+    width = 4,
+    activebackground = "gray",
 )
-btn14.pack(side = LEFT, expand = True, fill = "both")
+btn0.grid(row = 3, column = 1)
+btn0.bind("<Button-1>", buttonsClicked)                 #binding action for a button
 
-btn15 = Button(
-    btnrow4,
-    text = "+/-",
-    font = ("Roboto", 10), bg = "#000000", fg = "white",
+btnDot = Button(
+    btnFrame,
+    text = ".",
+    font = ("Roboto", 17), bg = "#000000", fg = "white",
     relief = GROOVE,
     border = 0,
-    command = buttonnegpos
+    width = 4,
+    activebackground = "gray"
 )
-btn15.pack(side = LEFT, expand = True, fill = "both")
+btnDot.grid(row = 3, column = 0)
+btnDot.bind("<Button-1>", buttonsClicked)
 
-btn16 = Button(
-    btnrow4,
+btnNegPos = Button(
+    btnFrame,
+    text = "±",
+    font = ("Roboto", 17), bg = "#000000", fg = "white",
+    relief = GROOVE,
+    border = 0,
+    width = 4,
+    activebackground = "gray"
+)
+btnNegPos.grid(row = 3, column = 2)
+btnNegPos.bind("<Button-1>", buttonsClicked)
+
+btnPlus = Button(
+    btnFrame,
+    text = "+",
+    font = ("Roboto", 17), bg = "#000000", fg = "white",
+    relief = GROOVE,
+    border = 0,
+    width = 4,
+    activebackground = "gray"
+)
+btnPlus.grid(row = 0, column = 3)
+btnPlus.bind("<Button-1>", buttonsClicked)
+
+btnMinus = Button(
+    btnFrame,
+    text = "-",
+    font = ("Roboto", 17), bg = "#000000", fg = "white",
+    relief = GROOVE,
+    border = 0,
+    width = 4,
+    activebackground = "gray"
+)
+btnMinus.grid(row = 1, column = 3)
+btnMinus.bind("<Button-1>", buttonsClicked)
+
+btnMultiply = Button(
+    btnFrame,
+    text = "*",
+    font = ("Roboto", 17), bg = "#000000", fg = "white",
+    relief = GROOVE,
+    border = 0,
+    width = 4,
+    activebackground = "gray"
+)
+btnMultiply.grid(row = 2, column = 3)
+btnMultiply.bind("<Button-1>", buttonsClicked)
+
+btnDivide = Button(
+    btnFrame,
     text = "/",
-    font = ("Roboto", 10), bg = "#000000", fg = "white",
+    font = ("Roboto", 17), bg = "#000000", fg = "white",
     relief = GROOVE,
     border = 0,
-    command = buttondivide
+    width = 4,
+    activebackground = "gray"
 )
-btn16.pack(side = LEFT, expand = True, fill = "both")
+btnDivide.grid(row = 3, column = 3)
+btnDivide.bind("<Button-1>", buttonsClicked)
 
-btn17 = Button(
-    btnrow5,
+btnClear = Button(
+    btnFrame,
     text = "C",
-    font = ("Roboto", 10), bg = "#000000", fg = "white",
+    font = ("Roboto", 17), bg = "#000000", fg = "white",
     relief = GROOVE,
     border = 0,
-    command = buttonc
+    width = 4,
+    activebackground = "gray",
+    command = buttonC
 )
-btn17.pack(side = LEFT, expand = True, fill = "both")
+btnClear.grid(row = 4, column = 0)
 
-btn18 = Button(
-    btnrow5,
-    text = "%",
-    font = ("Roboto", 10), bg = "#000000", fg = "white",
+btnPercent = Button(
+    btnFrame,
+    text = "pct",
+    font = ("Roboto", 17), bg = "#000000", fg = "white",
     relief = GROOVE,
     border = 0,
-    command = buttonpercent
+    width = 4,
+    activebackground = "gray"
 )
-btn18.pack(side = LEFT, expand = True, fill = "both")
+btnPercent.grid(row = 4, column = 1)
+btnPercent.bind("<Button-1>", buttonsClicked)
 
-btn19 = Button(
-    btnrow5,
-    text = "=",
-    font = ("Roboto", 10), bg = "#000000", fg = "white",
-    relief = GROOVE,
-    border = 0,
-    command = result
-)
-btn19.pack(side = LEFT, expand = True, fill = "both")
-
-btn20 = Button(
-    btnrow5,
+btnSquarert = Button(
+    btnFrame,
     text = "√",
-    font = ("Roboto", 10), bg = "#000000", fg = "white",
+    font = ("Roboto", 17), bg = "#000000", fg = "white",
     relief = GROOVE,
     border = 0,
-    command = buttonsquareroot
+    width = 4,
+    activebackground = "gray"
 )
-btn20.pack(side = LEFT, expand = True, fill = "both")
+btnSquarert.grid(row = 4, column = 2)
+btnSquarert.bind("<Button-1>", buttonsClicked)
+
+btnEqual = Button(
+    btnFrame,
+    text = "=",
+    font = ("Roboto", 17), bg = "#000000", fg = "white",
+    relief = GROOVE,
+    border = 0,
+    width = 4,
+    activebackground = "gray"
+)
+btnEqual.grid(row = 4, column = 3)
+btnEqual.bind("<Button-1>", buttonsClicked)
 
 root.mainloop()
